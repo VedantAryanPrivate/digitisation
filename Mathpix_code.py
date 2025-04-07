59f369b9079cf3521e1da9bc1056817f7d2b2153
Simran Naik <156887988+SimNaik@users.noreply.github.com>
2025-04-07 16:47:55 +0530

    Update and rename Mathpix_code to Mathpix_code.py

--- Mathpix_code.py
+++ Mathpix_code.py
@@ -35,3 +35,23 @@ if response.status_code == 200:
     print("File downloaded successfully as", pdf_id + ".mmd")
 else:
     print("Failed to download the file. Status code:", response.status_code)
+
+#to get only md 
+import requests
+
+pdf_id = "2023_12_05_309596025b04d4d734dbg"
+url = "https://api.mathpix.com/v3/pdf/" + pdf_id + ".md"
+headers = {
+    # Your headers here (if required by the API)
+    'app_id': 'webtech_allen_ac_in_b6eda4_55dc4b',
+    'app_key': 'a869e65df7d85c35385bcc8ca72f8c83a5423865f702df7d56e52cf8366d1113'
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    with open(pdf_id + ".md", "w") as f:
+        f.write(response.text)
+    print("File downloaded successfully as", pdf_id + ".md")
+else:
+    print("Failed to download the file. Status code:", response.status_code)
